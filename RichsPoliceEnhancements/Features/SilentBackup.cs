349716ef1a601c49dac47298424eb05d780a0c03
Rich Dunne <richdunne@yahoo.com>
2020-10-26 09:27:29 -0600

    Modified conditions in getting nearby vehicles.

--- RichsPoliceEnhancements/Features/SilentBackup.cs
+++ RichsPoliceEnhancements/Features/SilentBackup.cs
@@ -14,7 +14,7 @@ namespace RichsPoliceEnhancements
 
                 if (Game.LocalPlayer.Character.LastVehicle && PursuitOrCalloutActive)
                 {
-                    foreach (Vehicle policeVeh in Game.LocalPlayer.Character.GetNearbyVehicles(16).Where(v => v && v.IsPoliceVehicle && v != Game.LocalPlayer.Character.LastVehicle && v.DistanceTo2D(Game.LocalPlayer.Character.LastVehicle) <= 100f && v.IsSirenOn))
+                    foreach (Vehicle policeVeh in Game.LocalPlayer.Character.GetNearbyVehicles(16).Where(v => v && v.IsPoliceVehicle && v != Game.LocalPlayer.Character.CurrentVehicle && v != Game.LocalPlayer.Character.LastVehicle && v.DistanceTo2D(Game.LocalPlayer.Character.LastVehicle) <= 100f && v.IsSirenOn))
                     {
                         if (!Game.LocalPlayer.Character.LastVehicle.IsSirenOn)
                         {
