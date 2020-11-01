5a2d9bbb242bbddb59734ed1042832690e16d137
Rich Dunne <richdunne@yahoo.com>
2020-11-01 10:54:05 -0700

    Fixed TVI vehicle heading comparison check.

--- RichsPoliceEnhancements/Features/TVI.cs
+++ RichsPoliceEnhancements/Features/TVI.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 using Rage;
 using LSPD_First_Response.Mod.API;
@@ -18,7 +17,10 @@ namespace RichsPoliceEnhancements
                 {
                     Game.LogTrivial("[RPE TVI]: Player is in pursuit.");
                     var pursuit = Functions.GetActivePursuit();
-                    //GameFiber.Sleep(2000);
+                    if(pursuit == null)
+                    {
+                        return;
+                    }
                     var suspectVeh = Functions.GetPursuitPeds(pursuit).Where(p => p && p.IsAlive).FirstOrDefault().CurrentVehicle;
 
                     if (suspectVeh && suspectVeh.HasDriver && suspectVeh.Speed > 0 && suspectVeh.FuelLevel > 0 && suspectVeh.Health > 0)
@@ -36,23 +38,34 @@ namespace RichsPoliceEnhancements
         internal static void VehicleDisableCheck(LHandle pursuit, Vehicle suspectVeh)
         {
             Game.LogTrivial("[RPE TVI]: Looping for disabled vehicle");
+
             while (suspectVeh && suspectVeh.HasOccupants && Functions.IsPursuitStillRunning(pursuit))
             {
+                if (!Game.LocalPlayer.Character.IsAlive)
+                {
+                    Game.LogTrivial($"[RPE TVI]: Player is dead.");
+                    alreadyRunning = false;
+                    return;
+                }
+
                 if (!suspectVeh || !suspectVeh.Driver || !suspectVeh.Driver.IsAlive)
                 {
                     Game.LogTrivial($"[RPE TVI]: Suspect vehicle or driver is null or dead.");
                     alreadyRunning = false;
                     return;
                 }
+
                 var origHeading = suspectVeh.Heading;
-                //Game.LogTrivial("[RPE TVI]: TVI looping on suspect vehicle.");
-                foreach (Vehicle veh in suspectVeh.Driver.GetNearbyVehicles(16))
+                foreach (Vehicle veh in suspectVeh.Driver.GetNearbyVehicles(16).Where(x => x && x.IsPoliceVehicle && x.HasDriver))
                 {
                     //Game.LogTrivial("[RPE TVI]: Looping for cops in pursuit");
-                    if (veh && veh.IsPoliceVehicle && veh.HasDriver && Rage.Native.NativeFunction.Natives.IS_ENTITY_TOUCHING_ENTITY<bool>(veh, suspectVeh))
+                    if (Rage.Native.NativeFunction.Natives.IS_ENTITY_TOUCHING_ENTITY<bool>(veh, suspectVeh))
                     {
+                        var originalSuspectHandlingData = suspectVeh.HandlingData;
                         GameFiber.Sleep(2000);
-                        if (Math.Abs(origHeading - suspectVeh.Heading) > 110f && Math.Abs(origHeading - suspectVeh.Heading) < 360f)
+                        var headingDifference = getAngleFromHeadingDifference(origHeading, suspectVeh.Heading);
+                        Game.LogTrivial($"[RPE TVI]: Heading difference: {headingDifference}");
+                        if (headingDifference > 90 && headingDifference < 180)
                         {
                             if (Settings.EnablePursuitUpdates)
                             {
@@ -68,7 +81,13 @@ namespace RichsPoliceEnhancements
                     }
                 }
                 GameFiber.Yield();
+
+                float getAngleFromHeadingDifference(float originalHeading, float newHeading)
+                {
+                    return Math.Min((originalHeading - newHeading) < 0 ? originalHeading - newHeading + 360 : originalHeading - newHeading, (newHeading - originalHeading) < 0 ? newHeading - originalHeading + 360 : newHeading - originalHeading);
+}
             }
+            alreadyRunning = false;
         }
     }
 }
