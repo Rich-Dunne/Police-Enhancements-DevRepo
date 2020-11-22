4935a2157e72f78285b8ece89cbd39817add9465
Rich Dunne <richdunne@yahoo.com>
2020-11-22 05:48:31 -0700

    Refactored if statement conditions

--- RichsPoliceEnhancements/Features/SilentBackup.cs
+++ RichsPoliceEnhancements/Features/SilentBackup.cs
@@ -1,11 +1,13 @@
 ﻿using Rage;
 using LSPD_First_Response.Mod.API;
 using System.Linq;
+using System.Collections.Generic;
 
 namespace RichsPoliceEnhancements
 {
     internal class SilentBackup
     {
+        private static List<Vehicle> _backupUnits = new List<Vehicle>();
         internal static void Main()
         {
             while (true)
@@ -17,16 +19,16 @@ namespace RichsPoliceEnhancements
 
                 if (Game.LocalPlayer.Character.LastVehicle && (isCalloutRunning || isCurrentPulloverActive || isPursuitActive))
                 {
-                    foreach (Vehicle policeVeh in Game.LocalPlayer.Character.GetNearbyVehicles(16).Where(v => v && v.IsPoliceVehicle && v != Game.LocalPlayer.Character.CurrentVehicle && v != Game.LocalPlayer.Character.LastVehicle && v.DistanceTo2D(Game.LocalPlayer.Character.LastVehicle) <= 100f && v.IsSirenOn))
+                    foreach (Vehicle policeVeh in Game.LocalPlayer.Character.GetNearbyVehicles(16).Where(v => v && v.IsPoliceVehicle && v.HasDriver && v != Game.LocalPlayer.Character.LastVehicle && v.DistanceTo2D(Game.LocalPlayer.Character.LastVehicle) <= 100f && v.IsSirenOn))
                     {
                         //Game.LogTrivial($"[RPE Silent Backup]: Found nearby police vehicle with siren on.");
-                        if (!Game.LocalPlayer.Character.LastVehicle.IsSirenOn || (Game.LocalPlayer.Character.CurrentVehicle && !Game.LocalPlayer.Character.CurrentVehicle.IsSirenOn))
+                        if (!Game.LocalPlayer.Character.LastVehicle.IsSirenOn)
                         {
                             //Game.LogTrivial($"[RPE Silent Backup]:  Silencing nearby units");
                             policeVeh.IsSirenOn = false;
                             policeVeh.IsSirenSilent = true;
                         }
-                        else if(Game.LocalPlayer.Character.LastVehicle.IsSirenOn || (Game.LocalPlayer.Character.CurrentVehicle && Game.LocalPlayer.Character.CurrentVehicle.IsSirenOn))
+                        else if(Game.LocalPlayer.Character.LastVehicle.IsSirenOn)
                         {
                             //Game.LogTrivial($"[RPE Silent Backup]:  Enabling nearby units' sirens");
                             policeVeh.IsSirenOn = true;
