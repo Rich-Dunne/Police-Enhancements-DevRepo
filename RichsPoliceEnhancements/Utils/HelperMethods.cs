5f65a12705170e4a3bb087446b2f470abb6396ae
Rich Dunne <richdunne@yahoo.com>
2021-04-11 09:25:03 -0600

    Initial commit

---
+++ RichsPoliceEnhancements/Utils/HelperMethods.cs
@@ -0,0 +1,14 @@
+﻿using Rage;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RichsPoliceEnhancements.Utils
+{
+    internal static class HelperMethods
+    {
+        internal static List<Ped> GetReleventPedsForAmbientEvent() => World.GetAllPeds().Where(p => p.IsRelevantForAmbientEvent()).ToList();
+    }
+}
---
+++ RichsPoliceEnhancements/Utils/PlayAudioFiles.cs
@@ -0,0 +1,31 @@
+﻿using Rage;
+using LSPD_First_Response.Mod.API;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace RichsPoliceEnhancements.Utils
+{
+    internal static class PlayAudioFiles
+    {
+        internal static void Start()
+        {
+            string directory = Directory.GetCurrentDirectory() + "\\lspdfr\\audio\\scanner\\STREETS";
+            GameFiber.StartNew(() =>
+            {
+                string[] files = Directory.GetFiles(directory, "*.wav");
+                foreach(string file in files)
+                {
+                    Game.LogTrivial($"{file}");
+                    var streetAudio = $"{Path.GetFileNameWithoutExtension(file).Replace(" ", "_").ToUpper()}";
+                    Game.LogTrivial($"{streetAudio}");
+                    Functions.PlayScannerAudio(streetAudio);
+                    GameFiber.Sleep(3000);
+                }
+            }, "Play Audio Files Fiber");
+        }
+    }
+}
