56ed633cd76e86e0bb245b0fbe0ddca893a18d0f
Rich Dunne <richdunne@yahoo.com>
2021-09-03 05:56:06 -0600

    Updated IsAmbient method logic

--- RichsPoliceEnhancements/Utils/Extensions.cs
+++ RichsPoliceEnhancements/Utils/Extensions.cs
@@ -457,71 +457,74 @@ namespace RichsPoliceEnhancements.Utils
 	/// </summary>
 	internal static class Extensions
     {
-        /// <summary>Determines if a ped can be considered ambient.  Checks any type of ped by default.
-        /// </summary>
-        internal static bool IsAmbient(this Ped ped, PedType pedType = 0)
-        {
-            // Universal tasks (virtually all peds seem have this)
-            var taskAmbientClips = Rage.Native.NativeFunction.Natives.GET_IS_TASK_ACTIVE<bool>(ped, 38);
+		/// <summary>Determines if a ped can be considered ambient.  Checks any type of ped by default.
+		/// </summary>
+		internal static bool IsAmbient(this Ped ped, PedType pedType = 0)
+		{
+			// Universal tasks (virtually all peds seem have this)
+			var taskAmbientClips = Rage.Native.NativeFunction.Natives.GET_IS_TASK_ACTIVE<bool>(ped, 38);
 
-            // Universal on-foot tasks (virtually all ambient walking peds seem to have this)
-            var taskComplexControlMovement = Rage.Native.NativeFunction.Natives.GET_IS_TASK_ACTIVE<bool>(ped, 35);
+			// Universal on-foot tasks (virtually all ambient walking peds seem to have this)
+			var taskComplexControlMovement = Rage.Native.NativeFunction.Natives.GET_IS_TASK_ACTIVE<bool>(ped, 35);
 
-            // Universal in-vehicle tasks (virtually all ambient driver peds seem to have this)
-            var taskInVehicleBasic = Rage.Native.NativeFunction.Natives.GET_IS_TASK_ACTIVE<bool>(ped, 150);
-            var taskCarDriveWander = Rage.Native.NativeFunction.Natives.GET_IS_TASK_ACTIVE<bool>(ped, 151);
+			// Universal in-vehicle tasks (virtually all ambient driver peds seem to have this)
+			var taskInVehicleBasic = Rage.Native.NativeFunction.Natives.GET_IS_TASK_ACTIVE<bool>(ped, 150);
+			var taskCarDriveWander = Rage.Native.NativeFunction.Natives.GET_IS_TASK_ACTIVE<bool>(ped, 151);
 
-            // On-foot ambient tasks
-            var taskPolice = Rage.Native.NativeFunction.Natives.GET_IS_TASK_ACTIVE<bool>(ped, 58); // From ambient cop (non-freemode) walking around
-            var taskWanderingScenario = Rage.Native.NativeFunction.Natives.GET_IS_TASK_ACTIVE<bool>(ped, 100); // From ambient cop walking around
-            var taskUseScenario = Rage.Native.NativeFunction.Natives.GET_IS_TASK_ACTIVE<bool>(ped, 118); // From ambient cop standing still
-            var taskScriptedAnimation = Rage.Native.NativeFunction.Natives.GET_IS_TASK_ACTIVE<bool>(ped, 134); // From UB ped waiting for interaction
+			// On-foot ambient tasks
+			var taskPolice = Rage.Native.NativeFunction.Natives.GET_IS_TASK_ACTIVE<bool>(ped, 58); // From ambient cop (non-freemode) walking around
+			var taskWanderingScenario = Rage.Native.NativeFunction.Natives.GET_IS_TASK_ACTIVE<bool>(ped, 100); // From ambient cop walking around
+			var taskUseScenario = Rage.Native.NativeFunction.Natives.GET_IS_TASK_ACTIVE<bool>(ped, 118); // From ambient cop standing still
+			var taskScriptedAnimation = Rage.Native.NativeFunction.Natives.GET_IS_TASK_ACTIVE<bool>(ped, 134); // From UB ped waiting for interaction
 
-            // In-vehicle controlled tasks
-            var taskControlVehicle = Rage.Native.NativeFunction.Natives.GET_IS_TASK_ACTIVE<bool>(ped, 169); // From backup unit driving to player
+			// In-vehicle controlled tasks
+			var taskControlVehicle = Rage.Native.NativeFunction.Natives.GET_IS_TASK_ACTIVE<bool>(ped, 169); // From backup unit driving to player
 
-            // If ped relationship group does not contain "cop" then this extension doesn't apply
-            if (pedType == PedType.Cop && !ped.RelationshipGroup.Name.ToLower().Contains("cop"))
-            {
-                //Game.LogTrivial($"Ped does not belong to a cop relationship group.");
-                return false;
-            }
+			// If ped relationship group does not contain "cop" then this extension doesn't apply
+			if (pedType == PedType.Cop && !ped.RelationshipGroup.Name.ToLower().Contains("cop"))
+			{
+				//Game.LogTrivial($"Ped does not belong to a cop relationship group.");
+				return false;
+			}
 
-            // UB unit on-foot, waiting for interaction
-            if (ped.RelationshipGroup.Name == "UBCOP")
-            {
-                //Game.LogTrivial($"Cop is UB unit. (non-ambient)");
-                return false;
-            }
+			// Ped is in a vehicle
+			if (ped.CurrentVehicle)
+			{
+				// Ped has a wander driving task
+				if (taskCarDriveWander)
+				{
+					//Game.LogTrivial($"Ped has a wander driving task. (ambient)");
+					return true;
+				}
 
-            // Ped is in a vehicle
-            if (ped.CurrentVehicle)
-            {
-                return IsAmbientInVehicle(ped);
-            }
+				// If the ped is in a vehicle but doesn't have a driving task, then it's a passenger.  Check if the vehicle's driver has a driving wander task
+				if (ped.CurrentVehicle.Driver && ped.CurrentVehicle.Driver != ped)
+				{
+					var driverHasWanderTask = Rage.Native.NativeFunction.Natives.GET_IS_TASK_ACTIVE<bool>(ped.CurrentVehicle.Driver, 151);
+					if (driverHasWanderTask)
+					{
+						//Game.LogTrivial($"Ped is a passenger.  Vehicle's driver has a wander driving task. (ambient)");
+						return true;
+					}
+				}
+			}
 
-            // Cop ped walking around or standing still
-            if ((taskComplexControlMovement && taskWanderingScenario) || (taskAmbientClips && taskUseScenario))
-            {
-                //Game.LogTrivial($"Ped is wandering around or standing still. (ambient)");
-                return true;
-            }
+			if (ped.IsOnFoot)
+			{
+				// Cop ped walking around or standing still
+				if ((taskComplexControlMovement && taskWanderingScenario) || (taskAmbientClips && taskUseScenario))
+				{
+					//Game.LogTrivial($"Ped is wandering around or standing still. (ambient)");
+					return true;
+				}
+			}
 
-            // If nothing else returns true before now, then the ped is probably being controlled and doing something else
-            Game.LogTrivial($"[RPE Ambiance Check]: Nothing else has returned true by this point. (non-ambient)");
-            Game.LogTrivial($"[RPE Ambiance Check]: Ped's active tasks are:");
-            var tasks = (PedTask[])Enum.GetValues(typeof(PedTask));
-            foreach (PedTask task in tasks)
-            {
-                if (Rage.Native.NativeFunction.Natives.GET_IS_TASK_ACTIVE<bool>(ped, (int)task))
-                {
-                    Game.LogTrivial($"[RPE Ambiance Check]:		{task} ({(int)task})");
-                }
-            }
-            return false;
-        }
+			// If nothing else returns true before now, then the ped is probably being controlled and doing something else
+			//Game.LogTrivial($"Nothing else has returned true by this point. (non-ambient)");
+			return false;
+		}
 
-        private static bool IsAmbientInVehicle(Ped ped)
+		private static bool IsAmbientInVehicle(Ped ped)
         {
             // In-vehicle controlled tasks
             var taskControlVehicle = Rage.Native.NativeFunction.Natives.GET_IS_TASK_ACTIVE<bool>(ped, 169); // From backup unit driving to player
